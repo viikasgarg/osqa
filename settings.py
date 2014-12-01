e66501687c7ec98b304a2a56ec5d56662ba579fc
Vikas Garg <vgarg@castlighthealth.com>
2014-12-01 16:17:26 +0530

    forum changed

--- settings.py
+++ settings.py
@@ -70,10 +70,80 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # User settings
 
+import os.path
+
+SITE_SRC_ROOT = os.path.dirname(__file__)
+LOG_FILENAME = 'django.osqa.log'
+
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '%(pathname)s TIME: %(asctime)s MSG: %(filename)s:%(funcName)s:%(lineno)d %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'formatter': 'default',
+            'filename': os.path.join(SITE_SRC_ROOT, 'log', LOG_FILENAME),
+        },
+    },
+    'loggers' : {
+        # ensure that all log entries are propagated to root
+        'django': { 'propagate': True },
+        'django.request': { 'propagate': True },
+        'django.security': { 'propagate': True },
+        'py.warnings': { 'propagate': True },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'ERROR',
+    },
+}
+
+#ADMINS and MANAGERS
+ADMINS = ("viikasgarg@gmail.com")
+MANAGERS = ADMINS
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': True
+}
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+CACHE_BACKEND = 'file://%s' % os.path.join(os.path.dirname(__file__),'cache').replace('\\','/')
+#CACHE_BACKEND = 'dummy://'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
+
+
+# This should be equal to your domain name, plus the web application context.
+# This shouldn't be followed by a trailing slash.
+# I.e., http://www.yoursite.com or http://www.hostedsite.com/yourhostapp
+APP_URL = 'http://selflearning.herokuapp.com'
+
+#LOCALIZATIONS
+TIME_ZONE = 'America/New_York'
+
+#OTHER SETTINGS
+
+USE_I18N = True
+LANGUAGE_CODE = 'en'
+
+OSQA_DEFAULT_SKIN = 'default'
+
+DISABLED_MODULES = ['books', 'recaptcha', 'project_badges','mysqlfulltext']
+
+STATIC_URL = '/static/'
+
 try:
     from settings_local import *
 except:
     pass
+
 template_loaders = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
