d1d157b20a256d5535447fe014df3fe7e1369aac
Vikas Garg <vgarg@castlighthealth.com>
2014-12-12 15:57:53 +0530

    Google Auth Added

--- forum_modules/googleauth/authentication.py
+++ forum_modules/googleauth/authentication.py
@@ -13,33 +13,36 @@ from django.utils.translation import ugettext as _
 
 import settings
 
-from json import load as load_json
+from json import loads as load_json
 
 
 class googleAuthConsumer(AuthenticationConsumer):
 
     def prepare_authentication_request(self, request, redirect_to):
-        args = dict(#"225108411153-i28bb8kou45pfpudvc7kq54fqaoetbu6.apps.googleusercontent.com"
+        args = dict(
             client_id=settings.GOOGLE_API_KEY,
             redirect_uri="%s%s" % (django_settings.APP_URL, redirect_to),
             scope="email",
             response_type="code"
         )
-        print "there"
+
         google_api_authentication_url = "https://accounts.google.com/o/oauth2/auth?" + urlencode(args)
 
         return google_api_authentication_url
 
     def process_authentication_request(self, request):
-        try:
+
             redirect_uri = "%s%s" % (django_settings.APP_URL, reverse('auth_provider_done', prefix='/', kwargs={'provider': 'google'}))
             args = dict(client_id=settings.GOOGLE_API_KEY, redirect_uri=redirect_uri)
 
-            args["client_secret"] = settings.GOOGLE_APP_SECRET  #google APP Secret
+            args["client_secret"] = settings.GOOGLE_APP_SECRET #google APP Secret
 
             args["code"] = request.GET.get("code", None)
-            response = cgi.parse_qs(urlopen("https://accounts.google.com/o/oauth2/access_token?" + urlencode(args)).read())
-            access_token = response["access_token"][-1]
+            args["grant_type"]="authorization_code"
+
+
+            response = load_json(urlopen("https://www.googleapis.com/oauth2/v3/token", urlencode(args)).read())
+            access_token = response["access_token"]
 
 
             user_data = self.get_user_data(access_token)
@@ -51,18 +54,19 @@ class googleAuthConsumer(AuthenticationConsumer):
 
             # Return the association key
             return assoc_key
-        except Exception, e:
-            logging.error("Problem during google authentication: %s" % e)
-            raise InvalidAuthentication(_("Something wrond happened during google authentication, administrators will be notified"))
+        #except Exception, e:
+        #    logging.error("Problem during google authentication: %s" % e)
+        #    raise InvalidAuthentication(_("Something wrond happened during google authentication, administrators will be notified"))
 
     def get_user_data(self, access_token):
-        profile = load_json(urlopen("https://accounts.google.com/o/oauth2/me?" + urlencode(dict(access_token=access_token))))
+#        import pdb; pdb.set_trace()
+        profile = load_json(urlopen("https://www.googleapis.com/plus/v1/people/me?" + urlencode(dict(access_token=access_token))).read())
 
-        name = profile["name"]
+        name = profile["displayName"]
 
         # Check whether the length if the email is greater than 75, if it is -- just replace the email
         # with a blank string variable, otherwise we're going to have trouble with the Django model.
-        email = smart_unicode(profile['email'])
+        email = smart_unicode(profile['emails'][0]['value'])
         if len(email) > 75:
             email = ''
 
--- forum_modules/googleauth/settings.py
+++ forum_modules/googleauth/settings.py
@@ -3,7 +3,7 @@
 from forum.settings import EXT_KEYS_SET
 from forum.settings.base import Setting
 
-GOOGLE_API_KEY = Setting('GOOGLE_API_KEY', '', EXT_KEYS_SET, dict(
+GOOGLE_API_KEY = Setting('GOOGLE_API_KEY', "225108411153-i28bb8kou45pfpudvc7kq54fqaoetbu6.apps.googleusercontent.com", EXT_KEYS_SET, dict(
 label = "google API key",
 help_text = """
 Get this key at the <a href="https://console.developers.google.com/project/">google developers network</a> to enable
@@ -11,7 +11,7 @@ authentication in your site through google.
 """,
 required=False))
 
-GOOGLE_APP_SECRET = Setting('GOOGLE_APP_SECRET', '', EXT_KEYS_SET, dict(
+GOOGLE_APP_SECRET = Setting('GOOGLE_APP_SECRET', "6RA8sIEw1LY4qL2hV3WN1W5w", EXT_KEYS_SET, dict(
 label = "google APP secret",
 help_text = """
 This your google app secret that you'll get in the same place as the API key.
